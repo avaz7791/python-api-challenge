ddbb78ae67d0752f68dcb2c2121e138041f2fbc0
Amir Vazquez <j.amir.vazquez@gmai.com>
2021-01-02 14:57:15 -0800

    a
    s

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "2bf09c264630bd0a49d6d608d5307394"
+weather_api_key = "aa"
 
 # Google API Key
-g_key = "AIzaSyBOYWU09KmcEKoJovkGQJU0dLztomHVkns"
+g_key = "bb"
